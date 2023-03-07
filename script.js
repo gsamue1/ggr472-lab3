e34c43fe34c5bc9a688a2f9866c489f417f3f604
gsamue1 <122768087+gsamue1@users.noreply.github.com>
2023-03-07 15:32:13 -0500

    Complete graduated symbols
    Legend update and circle-colour update

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ map.on('load', () => {
         'data': 'https://raw.githubusercontent.com/gsamue1/ggr472-lab3/main/indoor-ice-rinks-data.geojson' //Raw content Github Link -- going forward will develop more comprehensible link for data in website development
     });
 
-    // //Adding Indoor Rink GeoJSON geometry to existing basemap with simple styling
+    //Adding Indoor Rink GeoJSON geometry to existing basemap with simple styling
     map.addLayer({
         'id': 'indoor-rinks-to', // unique id develoepd for layer
         'type': 'circle',
@@ -69,7 +69,14 @@ map.on('load', () => {
         'paint': {
             'circle-radius': 4, // point size
             'circle-stroke-width': 2, //outline width
-            'circle-color': 'red',
+            'circle-color': [
+                'match', // Match expression 
+                ['get', 'Ice Pad Size Category'], // GET expression retrieves property value from 'capacity' data field
+                'NHL(200X85)', '#f35c4b', // 
+                'Regular(185X85)', '#eded4f', //
+                'Undersized(less than 185X85)', '#e0890f',
+                '#918e8d'
+            ],
             'circle-stroke-color': 'black' //outline colour
         },
     });
@@ -102,30 +109,30 @@ document.getElementById('returnbutton').addEventListener('click', () => {
 });
 
 // UNUSED CODE -- CONFIGURING POP UPS GOING FORWARD
-//When a click event occurs on a feature in the places layer, open a popup at the
-//location of the feature, with description HTML from its properties.
-// map.on('click', 'indoor-rinks-to', (e) => {
-//     console.log(e);   //e is the event info triggered and is passed to the function as a parameter (e)
-//      //Explore console output using Google DevTools
-//     let provname = e.features[0].properties.Public_Name;
-//     console.log(Public_Name);
-//  });
+// When a click event occurs on a feature in the places layer, open a popup at the
+// Location of the feature, with description HTML from its properties.
+map.on('click', 'indoor-rinks-to', (e) => {
+    console.log(e);   //e is the event info triggered and is passed to the function as a parameter (e)
+     //Explore console output using Google DevTools
+    let Public_Name = e.features[0].properties.Public_Name;
+    console.log(Public_Name);
+ });
 
-// // Change the cursor to a pointer when the mouse is over the places layer.
-// map.on('mouseenter', 'indoor-rinks-to', () => {
-// map.getCanvas().style.cursor = 'pointer';
-// });
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'indoor-rinks-to', () => {
+map.getCanvas().style.cursor = 'pointer';
+});
  
-// // Change it back to a pointer when it leaves.
-// map.on('mouseleave', 'indoor-rinks-to', () => {
-// map.getCanvas().style.cursor = '';
-// });
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'indoor-rinks-to', () => {
+map.getCanvas().style.cursor = '';
+});
 
-// //Pop Up for Outdoor Rinks
-// map.on('click', 'indoor-rinks-to', (e) => {
-// new mapboxgl.Popup() //Declare new popup object on each click
-//     .setLngLat(e.lngLat) //Use method to set coordinates of popup based on mouse click location
-//     .setHTML("<b>Rink Name:</b> " + e.features[0].properties.Public_Name + "<br>" +
-//         "Rink Type: door") //Use click event properties to write text for popup
-//     .addTo(map); //Show popup on map
-// });
+// // //Pop Up for Outdoor Rinks
+// // map.on('click', 'indoor-rinks-to', (e) => {
+// // new mapboxgl.Popup() //Declare new popup object on each click
+// //     .setLngLat(e.lngLat) //Use method to set coordinates of popup based on mouse click location
+// //     .setHTML("<b>Rink Name:</b> " + e.features[0].properties.Public_Name + "<br>" +
+// //         "Rink Type: door") //Use click event properties to write text for popup
+// //     .addTo(map); //Show popup on map
+// // });
